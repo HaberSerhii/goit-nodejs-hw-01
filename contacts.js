32d7df6cd9290b5d7b890b1fa5327ed76fb8aff5
Serhii Haber <haberserhii@icloud.com>
2023-12-11 17:04:15 +0200

    upd

--- contacts.js
+++ contacts.js
@@ -1,28 +1,35 @@
-import fs from "fs/promises";
-import { nanoid } from "nanoid";
-import path from "path";
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <email>", "user email")
+  .option("-p, --phone <phone>", "user phone");
 
-const contactsPath = path.resolve("db", "contacts.json");
+program.parse(process.argv);
 
-export const listContacts = async () => {
-  const data = await fs.readFile(contactsPath);
-  return JSON.parse(data);
+const argv = program.opts();
+
+const contacts = require("./contacts.js");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+    case "get":
+      const contact = await contacts.getContactById(id);
+      return console.log(contact);
+    case "remove":
+      const delContact = await contacts.removeContact(id);
+      return console.log(delContact);
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      return console.log(newContact);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
 };
-export const getContactById = async (id) => {
-  const contacts = await listContacts();
-  const result = contacts.find((item) => item.id === id);
-  return result || null;
-};
-// export const removeContact = async (contactId) => {};
-export const addContact = async (name, email, phone) => {
-  const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    name: name,
-    email: email,
-    phone: phone,
-  };
-  contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact;
-};
+
+invokeAction(argv);
--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 import * as contactsService from "./contacts.js";
+import { program } from "commander";
+const { Command } = require("commander");
+const program = new Command();
+const argv = require("yargs").argv;
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+const argv = program.opts();
+
+program.parse(process.argv);
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await contactsService.listContacts();
-      console.log(allContacts);
+      console.table(allContacts);
     case "get":
       const getContact = await contactsService.getContactById(id);
       return console.log(getContact);
     case "add":
       const newContact = await contactsService.addContact(name, email, phone);
       return console.log(newContact);
+    case "del":
+      const deleteContact = await contactsService.removeContact(id);
+      return console.log(deleteContact);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
   }
 };
 
-// invokeAction({ action: "list" });
-// invokeAction({ action: "get", id: "rsKkOQUi80UsgVPCcLZZW" });
-invokeAction({ action: "add", name: "QQQ", email: "www", phone: "123" });
+invokeAction(argv);
